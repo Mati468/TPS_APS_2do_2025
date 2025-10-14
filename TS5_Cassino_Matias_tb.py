e7fd7f8118f4079cd66c8c87f60ae4a171db2125
Tomás Altimare Bercovich <tomasalti@gmail.com>
2025-10-14 20:56:12 -0300

    TS5 - Actualización

--- TS5_Cassino_Matias_tb.py
+++ TS5_Cassino_Matias_tb.py
@@ -34,10 +34,11 @@ from scipy import signal as sig
 import matplotlib.pyplot as plt  
 import scipy.io as sio
 from scipy.io.wavfile import write
+from scipy.signal import periodogram
 import matplotlib as mpl
 import scipy.stats as st
 import pandas as pd
-import sounddevice as sd
+# import sounddevice as sd
 
 #%% Funciones
 plt.close('all')
@@ -61,7 +62,14 @@ def welch(cant_promedio,vector,zp,fs,window):
     f_w, Px_w = sig.welch(vector, fs=fs, nperseg=nperseg, window=window, nfft=nfft)
     return f_w, Px_w
 
-def plot_periodograma(f,Px,f_wb,title):
+def periodograma(cant_promedio, vector, zp, fs):
+    N = vector.shape[0]
+    nperseg = N//cant_promedio
+    nfft = zp*nperseg
+    f_P, Px_P = periodogram(vector, fs = fs, nfft = nfft)
+    return f_P, Px_P
+
+def plot_periodograma(f,Px,f_wb,title, f0 = 0):
     # Periodograma
     plt.figure()
     plt.plot(f,Px)
@@ -69,7 +77,7 @@ def plot_periodograma(f,Px,f_wb,title):
     plt.title(title)
     plt.xlabel("Frecuencia [Hz]")
     plt.ylabel("PSD")
-    plt.xlim([0,f_wb + 1])
+    plt.xlim([f0,f_wb + 1])
     plt.grid()
     return
 
@@ -107,7 +115,7 @@ zp_ppg=1
 fs_cucaracha, wav_data_cucaracha = sio.wavfile.read('sonido/la cucaracha.wav')
 # plt.figure()
 # plt.plot(wav_data_cucaracha)
-sd.play(wav_data_cucaracha, fs_cucaracha)
+# sd.play(wav_data_cucaracha, fs_cucaracha)
 
 
 # Parámetros cucaracha
@@ -121,7 +129,7 @@ zp_cucaracha=2
 fs_prueba, wav_data_prueba = sio.wavfile.read('sonido/prueba psd.wav')
 # plt.figure()
 # plt.plot(wav_data_prueba)
-sd.play(wav_data_prueba, fs_prueba)
+# sd.play(wav_data_prueba, fs_prueba)
 
 # Parámetros prueba
 cant_promedio_prueba = 10 # K = 8
@@ -134,7 +142,7 @@ zp_prueba=1
 fs_silbido, wav_data_silbido = sio.wavfile.read('sonido/silbido.wav')
 # plt.figure()
 # plt.plot(wav_data_silbido)
-sd.play(wav_data_silbido, fs_silbido)
+# sd.play(wav_data_silbido, fs_silbido)
 
 # Parámetros silbido
 cant_promedio_silbido = 25 # K = 17
@@ -144,7 +152,7 @@ zp_silbido=3
 # Lectura de audio bonus#
 ####################
 fs_audio, wav_data_audio = sio.wavfile.read('sonido/knock.wav')
-sd.play(wav_data_audio, fs_audio)
+# sd.play(wav_data_audio, fs_audio)
 if wav_data_audio.ndim > 1:
 #     # promedio de canales (podés usar sum/2 o canal específico)
     wav_data_audio = wav_data_audio.mean(axis=1)
@@ -156,32 +164,54 @@ cant_promedio_audio = 15 # K
 zp_audio=2
 
 #%% Invocación de las funciones del punto 1 y Bonus
+ff_P_ECG_SR, P_ECG_SR = periodograma(cant_promedio = cant_promedio_ecg, vector=ecg_one_lead, fs = fs_ecg, zp = zp_ecg)
 f_w_ecg, Px_w_ecg=welch(cant_promedio=cant_promedio_ecg,vector=ecg_one_lead,zp=zp_ecg,fs=fs_ecg,window=windows[1])
+f_P_ECG = ancho_de_banda(ff_P_ECG_SR,P_ECG_SR,porcentaje=99)
 f_wb_ecg=ancho_de_banda(f_w_ecg,Px_w_ecg,porcentaje=99)
 
+ff_P_PPG, P_PPG = periodograma(cant_promedio = cant_promedio_ppg, vector=ppg, fs = fs_ppg, zp = zp_ppg)
 f_w_ppg, Px_w_ppg=welch(cant_promedio=cant_promedio_ppg,vector=ppg,zp=zp_ppg,fs=fs_ppg,window=windows[1])
+f_P_PPG = ancho_de_banda(ff_P_PPG,P_PPG,porcentaje=99)
 f_wb_ppg=ancho_de_banda(f_w_ppg,Px_w_ppg,porcentaje=99)
 
+ff_P_cucaracha, P_cucaracha = periodograma(cant_promedio = cant_promedio_cucaracha, vector=wav_data_cucaracha, fs = fs_cucaracha, zp = zp_cucaracha)
 f_w_cucaracha, Px_w_cucaracha=welch(cant_promedio=cant_promedio_cucaracha,vector=wav_data_cucaracha,zp=zp_cucaracha,fs=fs_cucaracha,window=windows[1])
+f_P_cucaracha = ancho_de_banda(ff_P_cucaracha,P_cucaracha,porcentaje=99)
 f_wb_cucaracha=ancho_de_banda(f_w_cucaracha,Px_w_cucaracha,porcentaje=99)
 
+ff_P_prueba, P_prueba = periodograma(cant_promedio = cant_promedio_prueba, vector=wav_data_prueba, fs = fs_prueba, zp = zp_prueba)
 f_w_prueba, Px_w_prueba=welch(cant_promedio=cant_promedio_prueba,vector=wav_data_prueba,zp=zp_prueba,fs=fs_prueba,window=windows[1])
+f_P_prueba = ancho_de_banda(ff_P_prueba,P_prueba,porcentaje=99)
 f_wb_prueba=ancho_de_banda(f_w_prueba,Px_w_prueba,porcentaje=99)
 
+ff_P_silbido, P_silbido = periodograma(cant_promedio = cant_promedio_silbido, vector=wav_data_silbido, fs = fs_silbido, zp = zp_silbido)
 f_w_silbido, Px_w_silbido=welch(cant_promedio=cant_promedio_silbido,vector=wav_data_silbido,zp=zp_silbido,fs=fs_silbido,window=windows[1])
+f_P_silbido = ancho_de_banda(ff_P_silbido,P_silbido,porcentaje=99)
 f_wb_silbido=ancho_de_banda(f_w_silbido,Px_w_silbido,porcentaje=99)
 
+ff_P_audio, P_audio = periodograma(cant_promedio = cant_promedio_audio, vector=wav_data_audio, fs = fs_audio, zp = zp_audio)
 f_w_audio, Px_w_audio=welch(cant_promedio=cant_promedio_audio,vector=wav_data_audio,zp=zp_audio,fs=fs_audio,window=windows[1])
+f_P_audio = ancho_de_banda(ff_P_audio,P_audio,porcentaje=99)
 f_wb_audio=ancho_de_banda(f_w_audio,Px_w_audio,porcentaje=99)
 
 #%% Invocación de las funciones del punto 2 y Bonus
 plot_periodograma(f_w_ecg, Px_w_ecg, f_wb_ecg, title="Periodograma ECG sin ruido por Welch")
-plot_periodograma(f_w_ppg, Px_w_ppg, f_wb_ppg, title="Periodograma PPG sin ruido por Welch")
-plot_periodograma(f_w_cucaracha, Px_w_cucaracha, f_wb_cucaracha, title="Periodograma audio cucaracha sin ruido por Welch")
-plot_periodograma(f_w_prueba, Px_w_prueba, f_wb_prueba, title="Periodograma audio prueba sin ruido por Welch")
-plot_periodograma(f_w_silbido, Px_w_silbido, f_wb_silbido, title="Periodograma audio silbido sin ruido por Welch")
-plot_periodograma(f_w_audio, Px_w_audio, f_wb_audio, title="Periodograma audio bonus por Welch")
+plot_periodograma(ff_P_ECG_SR, P_ECG_SR, f_P_ECG, title="Periodograma ECG sin ruido por método de Periodograma Modificado")
 
+plot_periodograma(f_w_ppg, Px_w_ppg, f_wb_ppg, title="Periodograma PPG sin ruido por Welch")
+plot_periodograma(ff_P_PPG, P_PPG, f_P_PPG, title="Periodograma PPG sin ruido por método de Periodograma Modificado")
+
+plot_periodograma(f_w_cucaracha, Px_w_cucaracha, f_wb_cucaracha, f0 = 500, title="Periodograma audio cucaracha sin ruido por Welch")
+plot_periodograma(ff_P_cucaracha, P_cucaracha, f_P_cucaracha, f0 = 500, title="Periodograma audio cucaracha sin ruido por método de Periodograma Modificado")
+
+plot_periodograma(f_w_prueba, Px_w_prueba, f_wb_prueba, title="Periodograma audio prueba sin ruido por Welch")
+plot_periodograma(ff_P_prueba, P_prueba, f_P_prueba, title="Periodograma audio prueba sin ruido por método de Periodograma Modificado")
+
+plot_periodograma(f_w_silbido, Px_w_silbido, f_wb_silbido, title="Periodograma audio silbido sin ruido por Welch")
+plot_periodograma(ff_P_silbido, P_silbido, f_P_silbido, title="Periodograma audio silbido sin ruido por método de Periodograma Modificado")
+
+plot_periodograma(f_w_audio, Px_w_audio, f_wb_audio, title="Periodograma audio bonus por Welch")
+plot_periodograma(ff_P_audio, P_audio, f_P_audio, title="Periodograma audio bonus por método de Periodograma Modificado")
 
 tabla_bw = pd.DataFrame({
     'Archivo de Señal': [
@@ -199,8 +229,16 @@ tabla_bw = pd.DataFrame({
         f_wb_prueba,
         f_wb_silbido,
         f_wb_audio
+    ],
+    'Ancho de Banda - Periodograma Modificado [Hz]': [
+        f_P_ECG,
+        f_P_PPG,
+        f_P_cucaracha,
+        f_P_prueba,
+        f_P_silbido,
+        f_P_audio
     ]
-}) # Tabla de 2 columnas (etiquetas) y 6 filas (listas)
+}) # Tabla de 3 columnas (etiquetas) y 6 filas (listas)
 
 print("\n=== Tabla comparativa de anchos de banda ===\n")
 print(tabla_bw.to_string(index=False))
